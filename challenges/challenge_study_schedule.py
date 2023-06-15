0cbabf68d9e07314442e7112a8d98515bbf5d481
Christian <chrisnebech@hotmail.com>
2023-06-15 17:43:00 -0300

    requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+    
+    count = 0
+    for start_time, end_time in permanence_period:
+        if not (isinstance(start_time, int) and isinstance(end_time, int)):
+            return None
+        
+        if start_time <= target_time <= end_time:
+            count += 1
+    
+    return count
+
