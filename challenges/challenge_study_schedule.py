1c2cd5ea651b5834c1ef8183ebb85034201876f5
Christian <chrisnebech@hotmail.com>
2023-06-16 15:49:36 -0300

    ajuste lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,13 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-    
+
     count = 0
     for start_time, end_time in permanence_period:
         if not (isinstance(start_time, int) and isinstance(end_time, int)):
             return None
-        
+
         if start_time <= target_time <= end_time:
             count += 1
-    
-    return count
 
+    return count
