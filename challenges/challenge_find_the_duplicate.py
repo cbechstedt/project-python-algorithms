15ae6869aed135171b5576226565b396cbc7b966
Christian <chrisnebech@hotmail.com>
2023-06-16 18:14:05 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    numbers_seen = set()
+    for num in nums:
+        if not isinstance(num, int) or num < 1:
+            return False
+        if num in numbers_seen:
+            return num
+        numbers_seen.add(num)
+
+    return False
